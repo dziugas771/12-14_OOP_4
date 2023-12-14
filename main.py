724832d6d874e974dff229c8992a2fb1d150a612
DziugasRR <dziugas.rub@gmail.com>
2023-12-14 18:42:44 +0200

    my first Contry file

---
+++ main.py
@@ -0,0 +1,15 @@
+# class Country:
+#     def __init__(self, country_name, population, area):
+#         self.country_name = country_name
+#         self.population = population
+#         self.area = area
+#     def get_big(self):
+#         self.is_big = self.check_if_big()
+#
+# Croatia = Country('Croatia', 3998589, 56594)
+# United_Kingdom = Country('United_Kingdom', 67838492, 242900)
+#
+# Croatia
+# print(Croatia.)
+
+print("Saliu duomenys")
